069b49757cb803e27f252ce184ff42f265a01fa0
juidth.huang <judithhuang@augmentum.com.cn>
2018-04-03 14:08:25 +0800

    money: get user info

--- webapp/app.js
+++ webapp/app.js
@@ -8,14 +8,15 @@ App({
   },
   getUserInfo:function(cb){
     var that = this
+  
     if(this.globalData.userInfo){
       typeof cb == "function" && cb(this.globalData.userInfo)
     }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
+      wx.openSetting({
+        success: () => {
           wx.getUserInfo({
-            success: function (res) {
+            success: function(res) {
+              console.log('userInfo', res)
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
             }
